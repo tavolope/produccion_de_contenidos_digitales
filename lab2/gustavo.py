df332a0ae5255d562e2dd9c420bd27b43e518d57
gustavo <juan@.gmail.com>
2023-03-20 11:53:06 -0600

    se agrego un nombre

---
+++ lab2/gustavo.py
@@ -0,0 +1,17 @@
+edad = 17
+print (edad)
+
+nombre = "gustavo"
+print (nombre)
+
+pi = 3.1416
+print (pi)
+
+van_a_ganar_el_ano = True
+print (van_a_ganar_el_ano)
+
+print (type(edad))
+print (type(nombre))
+print (type(pi))
+print (type(van_a_ganar_el_ano))
+
