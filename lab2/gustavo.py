e1688561b0cdb430fec9ca4a5073f947b52fe99d
gustavo <juan@.gmail.com>
2023-03-22 08:36:36 -0600

    se agrego una linea de texto

--- lab2/gustavo.py
+++ lab2/gustavo.py
@@ -1,8 +1,11 @@
 edad = 17
 print (edad)
+'''<3
+;)'''
 
 nombre = "gustavo"
 print (nombre)
+# esta variable indica el nombre
 
 pi = 3.1416
 print (pi)
@@ -15,3 +18,21 @@ print (type(nombre))
 print (type(pi))
 print (type(van_a_ganar_el_ano))
 
+DIAS_DEL_ANO = 365 
+print (DIAS_DEL_ANO)
+
+print (f"mi nombre es {nombre} y estoy mamado")
+
+num1 = 2
+num2 = 76
+resultado = num1+num2
+print (resultado)
+
+resultado2 = resultado - num1 
+print (resultado2)
+
+resultado3 = resultado2 * num2 
+print (resultado3)
+
+mod = 50 % 5
+print (mod)
